82e842546e5423a926793875e7f9f962c381e9b6
hiro <hiromhiro617@gmail.com>
2021-06-04 23:44:30 +0900

    corsを修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,18 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins ['https://skate-spot-pro.vercel.app', 'http://localhost:3000'].map(&:strip)
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
-
-  allow do
-    origins 'https://skate-spot-pro.vercel.app'
-
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
+ 
 end
